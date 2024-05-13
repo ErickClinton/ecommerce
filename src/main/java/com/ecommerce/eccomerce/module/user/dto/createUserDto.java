cc46b65eaa6d36592c5500bac51707a274871d0c
Erick Clinton <erickleite2009321@gmail.com>
2024-05-13 17:12:08 -0300

    create and save user

--- src/main/java/com/ecommerce/eccomerce/module/user/dto/createUserDto.java
+++ src/main/java/com/ecommerce/eccomerce/module/user/dto/createUserDto.java
@@ -1,2 +1,6 @@
-package com.ecommerce.eccomerce.module.user.dto;public interface createUserDto {
+package com.ecommerce.eccomerce.module.user.dto;
+
+public record CreateUserDto(
+        String email,String password, String name
+) {
 }
