6718ac52d6544a972c18ea6a8b2898f515f866fa
Erick Clinton <erickleite2009321@gmail.com>
2024-05-13 17:12:22 -0300

    create entity in Bd and jpaRepository for user

---
+++ src/main/java/com/ecommerce/eccomerce/module/user/entity/UsersEntity.java
@@ -0,0 +1,34 @@
+package com.ecommerce.eccomerce.module.user.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.UUID;
+
+@Entity(name = "users")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsersEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "email")
+    @Email(message = "please, put an email exist")
+    private String email;
+
+    @Column(name = "password")
+    @Length(min = 8,max = 100)
+    private String password;
+
+    @Column(name = "name")
+    private String name;
+}
---
+++ src/main/java/com/ecommerce/eccomerce/module/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.ecommerce.eccomerce.module.user.repository;
+
+import com.ecommerce.eccomerce.module.user.entity.UsersEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface UserRepository  extends JpaRepository<UsersEntity, UUID> {
+
+}
